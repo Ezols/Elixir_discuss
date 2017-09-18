5009de9c55fec9f5caea906712491e9088ca6a89
Ezols <Edvards.ozols@gmail.com>
2017-09-18 16:26:45 +0300

    Added warning message to topic form

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,14 +3,23 @@ defmodule Discuss.TopicController do
 
     alias Discuss.Topic
 
-    def new(conn, params) do
+    def new(conn, _params) do
         changeset = Topic.changeset(%Topic{}, %{})
-
+        
+        # show the form
         render conn, "new.html", changeset: changeset
             
     end
 
-    def create(conn, params) do
-        %{"topic" => topic} = params
+    def create(conn, %{"topic" => topic}) do
+        changeset = Topic.changeset(%Topic{}, topic)
+
+        case Repo.insert(changeset) do
+            # Record added, will return atom :ok and post
+            {:ok, post} -> IO.inspect(post)
+            # Record faild to add, will return atom :error and changeset
+            {:error, changeset} -> 
+                render conn, "new.html", changeset: changeset
+        end
     end
 end
